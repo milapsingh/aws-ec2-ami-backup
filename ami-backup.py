ff116bca408efb231b615f4f3c448e721039129d
Milap Singh <milap.singh@admins-MacBook-Pro-2.local>
2018-09-12 23:27:31 +0530

    done

--- ami-backup.py
+++ ami-backup.py
@@ -4,7 +4,7 @@ import sys
 import boto3
 ec = boto3.client('ec2')
 create_time = datetime.datetime.now()
-create_fmt = create_time.strftime('%Y-%m-%d %H:%M:%S')
+create_fmt = create_time.strftime('%Y-%m-%d')
 response=ec.describe_instances(
     Filters=[
         {
@@ -19,5 +19,15 @@ for reservation in (response["Reservations"]):
     for instance in reservation["Instances"]:
         instancelist.append(instance["InstanceId"])
         print(instance["InstanceId"])
-        AMIid = ec.create_image(InstanceId=instance["InstanceId"], Name=instance["InstanceId"] + " - from " + create_fmt, Description=instance["InstanceId"] + " created AMI of instance  from " + create_fmt, NoReboot=True, DryRun=False)
-print(instancelist)
+        AMIid = ec.create_image(InstanceId=instance["InstanceId"], Name=instance["InstanceId"] + " - from  " + create_fmt, Description=instance["InstanceId"] + " created AMI of instance  from " + create_fmt, NoReboot=True, DryRun=False)
+        print(AMIid)
+        ec.create_tags(
+            Resources=[
+                AMIid['ImageId'],
+            ],
+            Tags=[
+                {'Key': 'Name', 'Value': instance["InstanceId"]},
+                {'Key': 'created-at', 'Value': create_fmt },
+            ]
+        )
+print("Ami created")
